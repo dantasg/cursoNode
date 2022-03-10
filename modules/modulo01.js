89b067638df1502def74f9985d7399d537d9ab1b
Genival Dantas <genineto10@gmail.com>
2022-03-10 10:45:33 -0300

    '4e5secao'

---
+++ modules/modulo01.js
@@ -0,0 +1,11 @@
+console.log('Executando module-01.js');
+
+module.exports.oculta = () => {
+    console.log('Executando função oculta');
+}
+
+module.exports.executa = () => {
+    console.log('Executa a função executa!');
+}
+
+module.exports.welcome = `Bem vindo a modulo2`;
